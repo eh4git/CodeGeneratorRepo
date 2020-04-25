6a174154945ed768d37b50020131a76aab4732f2
erik hirsch <ehirsch760@gmail.com>
2020-04-24 21:09:39 -0700

    Homework 3 code generator

---
+++ script.js
@@ -0,0 +1,61 @@
+function writePassword() {
+
+  //prompt question and store answers
+  var totalChar = parseInt(prompt("How many Characters?"));
+  while (totalChar < 8 || totalChar > 128) {
+    //newPassword = 'Password must contain atleast 8 characters and have at most 128'//this is not working right has undefinded after text
+    alert('Password must contain atleast 8 characters and have at most 128');
+    totalChar = parseInt(prompt("How many Characters?"));
+  }
+  var wantUpperCase = confirm("Uppercase?");
+  var wantLowerCase = confirm("Lowercase?");
+  var wantSpecialChar = confirm("Special Characters?")
+
+
+
+  console.log('totalChar:', totalChar);
+  console.log('wantUpperCase:', wantUpperCase);
+  console.log('wantLowerCase:', wantLowerCase);
+  console.log("wantSpecialChar: ", wantSpecialChar);
+  //create options arrays
+  var upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialChar = ["!", "#", "$", "%", "&", "*", "+", "-", "<", "=", ">", "?", "@", "^", "~"]
+  //initialize variables you need to create and store new password
+  var lettersArr = [];
+  var newPassword = "";
+  //filter through conditional based on user answers
+
+  if (totalChar >= 8 && totalChar <= 128 && wantUpperCase) {
+    lettersArr = lettersArr.concat(upperCaseLetter);
+  }
+  if (totalChar >= 8 && totalChar <= 128 && wantLowerCase) {
+    lettersArr = lettersArr.concat(lowerCaseLetter);
+  }
+  if (totalChar >= 8 && totalChar <= 128 && wantSpecialChar) {
+    lettersArr = lettersArr.concat(specialChar);
+  }
+  console.log('New lettersArr:', lettersArr);
+
+  //start of function to generate password
+  //  function writePassword(password){
+  //loop totalChar x times to randonly get random value from letters array
+  for (i = 0; i < totalChar; i++) {
+    //get random between 0-total-length of letterArr
+    var randomIndexNumber = Math.floor(Math.random() * lettersArr.length);
+    console.log('randomIndexNumber: ', randomIndexNumber);
+    //concatonate picked letters
+    newPassword = newPassword + lettersArr[randomIndexNumber];
+    console.log('newPassword: ', newPassword);
+  }
+  console.log('final answer: ', newPassword);
+  
+  //update the text area with new password
+  document.querySelector("#password").value = newPassword;
+  // 
+}
+
+
+
+// Add event listener to generate button ****Partially Working****
+generate.addEventListener("click", writePassword);
